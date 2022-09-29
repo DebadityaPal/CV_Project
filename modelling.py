f808c3a6c7fe92ea19400273ad1ee3d4bb6af736
Debaditya Pal <debaditya@sogage.com>
2022-09-29 15:57:44 +0530

    Fixed train.py

--- modelling.py
+++ modelling.py
@@ -34,16 +34,16 @@ class GAN_Generator(Module):
         self.maxpool1 = MaxPool2d(2)
         self.conv2 = ConvBlock(64, 128)
         self.maxpool2 = MaxPool2d(2)
-        self.conv3 = ConvBlock(128, 256)
+        self.conv3 = ConvBlock(128, 128)
         self.maxpool3 = MaxPool2d(2)
-        self.conv4 = ConvBlock(256, 512)
+        self.conv4 = ConvBlock(128, 128)
         self.maxpool4 = MaxPool2d(2)
 
         # Upsampling
         self.upsample1 = Upsample(scale_factor=2)
-        self.conv5 = Conv2d(512, 256, kernel_size=3, padding='same')
+        self.conv5 = Conv2d(128, 128, kernel_size=3, padding='same')
         self.upsample2 = Upsample(scale_factor=2)
-        self.conv6 = Conv2d(256, 128, kernel_size=3, padding='same')
+        self.conv6 = Conv2d(128, 128, kernel_size=3, padding='same')
         self.upsample3 = Upsample(scale_factor=2)
         self.conv7 = Conv2d(128, 64, kernel_size=3, padding='same')
         self.upsample4 = Upsample(scale_factor=2)
@@ -52,6 +52,7 @@ class GAN_Generator(Module):
     def generate_attention_mask(self, image):
         # Extract illumination channel from RGB
         illumination_channel = torch.amax(image, axis=1)
+        illumination_channel = illumination_channel.unsqueeze(1)
         # Normalize illumination channel
         illumination_channel = illumination_channel / \
             torch.max(illumination_channel)
@@ -59,73 +60,51 @@ class GAN_Generator(Module):
         attention_mask = 1 - illumination_channel
         return attention_mask
 
-    def PILtoTensor(self, image):
-        # Convert PIL image to tensor
-        image = transforms.ToTensor()(image)
-        image = image.unsqueeze(0)
-        return image
-
-    def TensortoPIL(self, image):
-        # Convert tensor to PIL image
-        image = transforms.ToPILImage()(image)
-        return image
-
     def resize(self, img, width, height):
         # Resize attension mask
         output = transforms.Resize((width, height))(img)
         return output
 
     def forward(self, inputs):
-        outputs = []
-        for image in inputs:
-            input_image = self.PILtoTensor(image).to(device)
-            # Generating attention mask
-            att_mask = self.generate_attention_mask(input_image)
+        # Extract attention mask
+        att_mask = self.generate_attention_mask(inputs)
+        # Downsampling
+        x = self.conv1(inputs)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = self.maxpool2(x)
+        x = self.conv3(x)
+        x = self.maxpool3(x)
+        x = self.conv4(x)
+        x = self.maxpool4(x)
 
-            # Downsampling
-            x = self.conv1(input_image)
-            x = self.maxpool1(x)
-            x = self.conv2(x)
-            x = self.maxpool2(x)
-            x = self.conv3(x)
-            x = self.maxpool3(x)
-            x = self.conv4(x)
-            x = self.maxpool4(x)
+        # Upsampling
+        x = x * self.resize(att_mask, x.shape[2], x.shape[3])
+        x = self.upsample1(x)
+        x = self.conv5(x)
+        x = x * self.resize(att_mask, x.shape[2], x.shape[3])
+        x = self.upsample2(x)
+        x = self.conv6(x)
+        x = x * self.resize(att_mask, x.shape[2], x.shape[3])
+        x = self.upsample3(x)
+        x = self.conv7(x)
+        x = x * self.resize(att_mask, x.shape[2], x.shape[3])
+        x = self.upsample4(x)
+        x = self.conv8(x)
 
-            # Upsampling
-            x = x * self.resize(att_mask, x.shape[2], x.shape[3])
-            x = self.upsample1(x)
-            x = self.conv5(x)
-            x = x * self.resize(att_mask, x.shape[2], x.shape[3])
-            x = self.upsample2(x)
-            x = self.conv6(x)
-            x = x * self.resize(att_mask, x.shape[2], x.shape[3])
-            x = self.upsample3(x)
-            x = self.conv7(x)
-            x = x * self.resize(att_mask, x.shape[2], x.shape[3])
-            x = self.upsample4(x)
-            x = self.conv8(x)
+        # Adding input image to the output
+        x = torch.unsqueeze(self.resize(
+            x[0], inputs.shape[2], inputs.shape[3]), 0)
+        x = x * att_mask
+        x = x + inputs
 
-            # Remove Batch Dimension
-            x = torch.squeeze(x, 0)
-            x = self.resize(x, input_image.shape[2], input_image.shape[3])
-            # Adding input image to the output
-            x = x * att_mask
-            x = x + torch.squeeze(input_image, 0)
-
-            # Converting tensor to PIL image
-            x = self.TensortoPIL(x)
-            outputs.append(x)
-
-        return outputs
+        return x
 
 
 class GAN_Discriminator(Module):
     def __init__(self):
         super().__init__()
-        self.weights = VGG16_Weights.IMAGENET1K_V1
-        self.vgg16 = models.vgg16(weights=self.weights)
-        self.preprocess = self.weights.transforms()
+        self.vgg16 = models.vgg16(weights='IMAGENET1K_V1')
         # Set requires_grad to False
         for param in self.vgg16.parameters():
             param.requires_grad = False
@@ -135,11 +114,6 @@ class GAN_Discriminator(Module):
         # Set Last layer to 1 class
         self.vgg16.classifier[6] = Linear(4096, 1)
 
-    def process(self, image):
-        image = self.preprocess(image)
-        return image
-
     def forward(self, x):
-        images = torch.stack([self.process(image) for image in x]).to(device)
-        outputs = self.vgg16(images)
+        outputs = self.vgg16(x)
         return outputs
--- train.py
+++ train.py
@@ -4,19 +4,64 @@ from tqdm import tqdm
 from torch.utils.data import DataLoader
 from dataloader import create_train_test_eval_sets, GANDataset
 from modelling import GAN_Generator, GAN_Discriminator
-import PIL
+import cv2
+from torchvision import transforms
 
 
 def collate(batch):
     # Load images and create batch
     images = []
     ground_truths = []
+    shapes = []
     for image, ground_truth in batch:
-        image = PIL.Image.open(image)
-        ground_truth = PIL.Image.open(ground_truth)
+        image = cv2.imread(image)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        ground_truth = cv2.imread(ground_truth)
+        ground_truth = cv2.cvtColor(ground_truth, cv2.COLOR_BGR2RGB)
+        image = torch.from_numpy(image).permute(2, 0, 1).float()
+        ground_truth = torch.from_numpy(ground_truth).permute(2, 0, 1).float()
         images.append(image)
         ground_truths.append(ground_truth)
-    return images, ground_truths
+        shapes.append(image.shape)
+    return images, ground_truths, shapes
+
+
+def preprocess_vgg16(images):
+    image_tensors = torch.zeros((len(images), 3, 224, 224))
+    preprocess = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
+                             0.229, 0.224, 0.225]),
+    ])
+    for i, image in enumerate(images):
+        image = preprocess(image)
+        image_tensors[i] = image
+    return image_tensors
+
+
+def remove_padding(images, shapes):
+    output = []
+    for i, image in enumerate(images):
+        output.append(image[:, :shapes[i][1], :shapes[i][2]])
+    return output
+
+
+def pad_images(images, ground_truths):
+    max_height = max([image.shape[1] for image in images])
+    max_width = max([image.shape[2] for image in images])
+    # Create tensor of zeros
+    padded_images = torch.zeros((len(images), 3, max_height, max_width))
+    padded_ground_truths = torch.zeros(
+        (len(ground_truths), 3, max_height, max_width))
+    for i, (image, ground_truth) in enumerate(zip(images, ground_truths)):
+        padded_images[i, :, :image.shape[1], :image.shape[2]] = image
+        padded_ground_truths[i, :, :ground_truth.shape[1],
+                             :ground_truth.shape[2]] = ground_truth
+    # Create masks
+    masks = torch.zeros((len(images), 1, max_height, max_width))
+    for i, (image, ground_truth) in enumerate(zip(images, ground_truths)):
+        masks[i, :, :image.shape[1], :image.shape[2]] = 1
+    return padded_images, padded_ground_truths, masks
 
 
 def main(args):
@@ -25,8 +70,8 @@ def main(args):
 
     train_dataset, _, val_dataset, train_labels, _, val_labels = create_train_test_eval_sets(
         exposure_errors_path=r'D:\Academics\CV_project\datasets\Exposure-Errors',
-        # lol_path=r'D:\Academics\CV_project\datasets\LOL',
-        # uieb_path=r'D:\Academics\CV_project\datasets\UIEB'
+        lol_path=r'D:\Academics\CV_project\datasets\LOL',
+        uieb_path=r'D:\Academics\CV_project\datasets\UIEB'
     )
     # Print length of datasets
     print("Train dataset length: {}".format(len(train_dataset)))
@@ -39,10 +84,10 @@ def main(args):
     train_dataset = GANDataset(train_dataset, train_labels)
     val_dataset = GANDataset(val_dataset, val_labels)
 
-    train_dataloader = DataLoader(
-        train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate)
-    val_dataloader = DataLoader(
-        val_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate)
+    # train_dataloader = DataLoader(
+    #     train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate)
+    # val_dataloader = DataLoader(
+    #     val_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate)
 
     generator = GAN_Generator().to(device)
     discriminator = GAN_Discriminator().to(device)
@@ -52,96 +97,127 @@ def main(args):
     discriminator_optimizer = torch.optim.Adam(
         discriminator.parameters(), lr=args.lr)
 
+    # Loss history
+    train_generator_loss_history = []
+    train_discriminator_loss_history = []
     # Training Loop begins here
     for epoch in range(args.epochs):
         generator.train()
         discriminator.train()
-        with tqdm(train_dataloader, unit='batch', leave=True, position=0) as pbar:
-            for images, ground_truths in pbar:
+        avg_train_generator_loss = 0
+        avg_train_discriminator_loss = 0
+        with tqdm(train_dataset, unit='batch', leave=True, position=0) as pbar:
+            for image, ground_truth in pbar:
+                # Load images
+                image = cv2.imread(image)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                ground_truth = cv2.imread(ground_truth)
+                ground_truth = cv2.cvtColor(ground_truth, cv2.COLOR_BGR2RGB)
+                image = torch.from_numpy(image).permute(2, 0, 1).float()
+                ground_truth = torch.from_numpy(ground_truth).permute(
+                    2, 0, 1).float()
+                image = image.unsqueeze(0)
+                ground_truth = ground_truth.unsqueeze(0)
                 ##################################
                 # (1) Update Discriminator network
                 ##################################
                 # Pass real images through discriminator
                 discriminator_optimizer.zero_grad()
-                real_output = discriminator(ground_truths)
+                discriminator_inputs = preprocess_vgg16(
+                    ground_truth).to(device)
+                real_output = discriminator(discriminator_inputs)
                 real_labels = torch.full(
-                    (args.batch_size, 1), 1, dtype=torch.float, device=device)
+                    (1, 1), 1, dtype=torch.float, device=device)
                 loss_fct = torch.nn.BCEWithLogitsLoss()
                 real_loss = loss_fct(real_output, real_labels)
-                real_loss.backward()
 
                 # Create fake images
-                fake_images = generator(images)
-                fake_output = discriminator(fake_images)
+                fake_image = generator(image.to(device))
+                fake_output = discriminator(fake_image.detach())
                 fake_labels = torch.full(
-                    (args.batch_size, 1), 0, dtype=torch.float, device=device)
+                    (1, 1), 0, dtype=torch.float, device=device)
                 loss_fct = torch.nn.BCEWithLogitsLoss()
                 fake_loss = loss_fct(fake_output, fake_labels)
-                fake_loss.backward()
 
                 # Update discriminator
-                discriminator_loss = real_loss.item() + fake_loss.item()
+                discriminator_loss = real_loss + fake_loss
+                discriminator_loss.backward()
                 discriminator_optimizer.step()
 
                 ##################################
                 # (2) Update Generator network
                 ##################################
                 generator_optimizer.zero_grad()
-                fake_output = discriminator(fake_images)
+                fake_output = discriminator(fake_image)
                 fake_labels = torch.full(
-                    (args.batch_size, 1), 1, dtype=torch.float, device=device)
+                    (1, 1), 1, dtype=torch.float, device=device)
                 loss_fct = torch.nn.BCEWithLogitsLoss()
                 generator_discriminator_loss = loss_fct(
                     fake_output, fake_labels)
-                generator_discriminator_loss.backward()
-                # generator_pixelwise_loss = torch.nn.functional.mse_loss(
-                #     fake_images, ground_truths)
-                # generator_pixelwise_loss.backward()
-                # generator_loss = generator_discriminator_loss.item() + \
-                #     generator_pixelwise_loss.item()
+                generator_pixelwise_loss = torch.nn.functional.mse_loss(
+                    fake_image, ground_truth.to(device))
+                generator_loss = generator_discriminator_loss + \
+                    generator_pixelwise_loss
+                generator_loss.backward()
                 generator_optimizer.step()
 
+                avg_train_generator_loss += generator_loss.item() / len(train_dataset)
+                avg_train_discriminator_loss += discriminator_loss.item() / len(train_dataset)
+
                 pbar.set_description(f'Train Epoch {epoch}')
-                # pbar.set_postfix(generator_loss=generator_loss.item(
-                # ), discriminator_loss=discriminator_loss.item())
-                torch.cuda.empty_cache()
+                pbar.set_postfix(generator_loss=generator_loss.item(
+                ), discriminator_loss=discriminator_loss.item())
+
+        train_generator_loss_history.append(avg_train_generator_loss)
+        train_discriminator_loss_history.append(avg_train_discriminator_loss)
 
         generator.eval()
         discriminator.eval()
         with torch.no_grad():
-            with tqdm(val_dataloader, unit='batch', leave=True, position=0) as pbar:
-                for images, ground_truths in pbar:
-                    fake_images = generator(images)
+            with tqdm(val_dataset, unit='batch', leave=True, position=0) as pbar:
+                for image, ground_truth in pbar:
+                    # Load images
+                    image = cv2.imread(image)
+                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                    ground_truth = cv2.imread(ground_truth)
+                    ground_truth = cv2.cvtColor(
+                        ground_truth, cv2.COLOR_BGR2RGB)
+                    image = torch.from_numpy(image).permute(2, 0, 1).float()
+                    ground_truth = torch.from_numpy(ground_truth).permute(
+                        2, 0, 1).float()
+                    image = image.unsqueeze(0).to(device)
+                    ground_truth = ground_truth.unsqueeze(0).to(device)
+                    fake_image = generator(image)
                     fake_labels = torch.full(
-                        (args.batch_size, 1), 0, dtype=torch.float, device=device)
+                        (1, 1), 0, dtype=torch.float, device=device)
                     real_labels = torch.full(
-                        (args.batch_size, 1), 1, dtype=torch.float, device=device)
+                        (1, 1), 1, dtype=torch.float, device=device)
                     # Combine real and fake images
-                    combined_images = ground_truths + fake_images
+                    combined_images = torch.cat((ground_truth, fake_image))
                     discriminator_labels = torch.cat(
                         (real_labels, fake_labels))
                     generator_labels = torch.cat((fake_labels, real_labels))
                     # Pass through discriminator
-                    combined_output = discriminator(combined_images)
+                    discriminator_inputs = preprocess_vgg16(
+                        combined_images).to(device)
+                    combined_output = discriminator(discriminator_inputs)
                     loss_fct = torch.nn.BCEWithLogitsLoss()
                     discriminator_loss = loss_fct(
                         combined_output, discriminator_labels)
                     generator_discriminator_loss = loss_fct(
                         combined_output, generator_labels)
-                    # generator_pixelwise_loss = torch.nn.functional.mse_loss(
-                    #     fake_images, ground_truths)
-                    # generator_loss = generator_discriminator_loss.item() + \
-                    #     generator_pixelwise_loss.item()
+                    generator_pixelwise_loss = torch.nn.functional.mse_loss(
+                        fake_image, ground_truth)
+                    generator_loss = generator_discriminator_loss + \
+                        generator_pixelwise_loss
                     pbar.set_description(f'Val Epoch {epoch}')
-                    # pbar.set_postfix(generator_loss=generator_loss.item(),
-                    #                  discriminator_loss=discriminator_loss.item())
-                    torch.cuda.empty_cache()
+                    pbar.set_postfix(generator_loss=generator_loss.item(),
+                                     discriminator_loss=discriminator_loss.item())
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--epochs', type=int, default=10)
-    parser.add_argument('--batch_size', type=int, default=2)
+    parser.add_argument('--epochs', type=int, default=5)
     parser.add_argument('--lr', type=float, default=0.001)
     args = parser.parse_args()
 
